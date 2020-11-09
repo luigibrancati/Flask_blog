b4d6ea0681199c7f790bdb8a2b18ce8adbb10852
Luigi Brancati <luigi.brancati93@gmail.com>
2020-11-09 23:21:40 +0100

    Fixed config with file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask
-from config import Config
 
 def create_app(test_config=None):
     #create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(Config)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
 
-    if test_config in not None:
+    if test_config is None:
+        app.config.from_pyfile('config.py',silent=True)
+    else:
         #load the test config if passed in
         app.config.from_mapping(test_config)
     
