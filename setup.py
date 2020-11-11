ed2c17ceb7179386139ed0cd67a4a5b8046b8ca7
Luigi Brancati <luigi.brancati93@gmail.com>
2020-11-11 13:32:43 +0100

    Fixed stuff

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 config = {
     'name': 'flaskr-lb',
-    'version': '1.0.2a',
+    'version': '1.1.1a',
     'packages': find_packages(),
     'include_package_data': True,
     'zip_safe': False,
-    'install_requires': ['flask',]
+    'install_requires': ['flask','flask-wtf','flask-login','flask-sqlalchemy','flask-migrate','email-validator']
 }
 
 setup(**config)
