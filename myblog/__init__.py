4104781252b51a715e0e31b491ce76882ec8b508
Luigi Brancati <luigi.brancati93@gmail.com>
2022-02-13 00:29:23 +0100

    Fixed logging for files

--- myblog/__init__.py
+++ myblog/__init__.py
@@ -25,11 +25,12 @@ def create_app(test_config=None):
     file_handler.setFormatter(
         logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
     )
-    app.logger.addHandler(file_handler)
     if app.debug:
-        app.logger.setLevel(logging.DEBUG)
+        file_handler.setLevel(logging.DEBUG)
     else:
-        app.logger.setLevel(logging.INFO)
+        file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
     app.logger.info('myblog startup')
 
     # CONFIG
